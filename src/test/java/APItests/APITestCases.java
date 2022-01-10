6f5a0d517be56b8628bd917f91696841bfbb1c78
Tetyana <tanyarunko@gmail.com>
2022-01-09 21:21:52 -0500

    API_Tetyana_token updated

--- src/test/java/APItests/APITestCases.java
+++ src/test/java/APItests/APITestCases.java
@@ -30,10 +30,11 @@ public class APITestCases {
 
     String email = new Faker().internet().emailAddress();
     String password = "tom2022";
-    String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL2xvYW5cL2FwaSIsImF1ZCI6Imh0dHA6XC9cL2xvY2FsaG9zdFwvbG9hblwvYXBpIiwiaWF0IjoxNjQxNzQzOTUxLCJleHAiOjE2NDE3NDc1NTEsImRhdGEiOnsidXNlcl9pZCI6IjM1NjYiLCJ0eXBlIjoiMiJ9fQ.B7IVW-NUP5GyCS1q54sAtuj2VeK3OtTEehOq9AcnvKE";
+    //String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL2xvYW5cL2FwaSIsImF1ZCI6Imh0dHA6XC9cL2xvY2FsaG9zdFwvbG9hblwvYXBpIiwiaWF0IjoxNjQxNzQ5ODI2LCJleHAiOjE2NDE3NTM0MjYsImRhdGEiOnsidXNlcl9pZCI6IjM1NjYiLCJ0eXBlIjoiMiJ9fQ.bz4DbO3e189tqPjoYd6XC3EC9aVqsv0APaz5GMfemr8";
     String jwtToken;
     String api_loginEmail = ConfigReader.getProperty("api_loginEmail");
     String api_loginPassword = ConfigReader.getProperty("api_loginPass");
+    String token = ConfigReader.getProperty("token");
 
     @BeforeClass
     public static void setupBaseUri() {
@@ -82,7 +83,7 @@ public class APITestCases {
 
 
     @Test
-    public void getRequestForMortgageApplicationValidAdminCredentials() {
+    public void GetRequestForMortgageApplicationValidAdminCredentials() {
         //  String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL2xvYW5cL2FwaSIsImF1ZCI6Imh0dHA6XC9cL2xvY2FsaG9zdFwvbG9hblwvYXBpIiwiaWF0IjoxNjQxNTc0NDExLCJleHAiOjE2NDE1NzgwMTEsImRhdGEiOnsidXNlcl9pZCI6IjM1NjYiLCJ0eXBlIjoiMiJ9fQ.JtiKZs6VXyN005QjnSSB7DcRXpqqcORDrTfQUPZj1B4";
         given().
                 header("Authorization", token).
